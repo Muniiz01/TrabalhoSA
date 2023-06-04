d515dffea1236e27f6b8c5e0a30a1f973915a5d5
Muniiz01 <victormuniz281@outlook.com>
2023-06-04 19:18:03 -0300

    finalmente, agora eu vou pro lolzinho

--- Nova pasta/script/compra.js
+++
@@ -1,8 +0,0 @@
-var btn2 = document.querySelector("#inputButton2");
-
-window.onload = ("click", function () {
-    var paragrafo = document.querySelector("#nomeProduto");
-    var paragrafo2 = document.querySelector("#precoProduto");
-    paragrafo.textContent =  localStorage.getItem('valueText1');
-    paragrafo2.textContent =  localStorage.getItem('valueText2');
-})
--- Nova pasta/script/index.js
+++ Nova pasta/script/index.js
@@ -1,18 +1,9 @@
 
-function abrir() {
-    document.getElementById('popup').style.display = 'block'
-}
-function fechar() {
-    document.getElementById('popup').style.display = 'none'
-}
+
 
 function login() {
     window.location.href = "login.html"
 }
 function cadastro() {
     window.location.href = "cadastro.html"
-}
-
-function alerta(){
-    alert('oiiii')
 }
